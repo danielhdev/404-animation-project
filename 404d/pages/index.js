d159fa4eb7618c648798fbb8afa534e3b4253955
danielhdev <d.huebschmann@tutanota.com>
2022-09-25 15:41:21 +0200

    update So

--- 404d/pages/index.js
+++ 404d/pages/index.js
@@ -3,7 +3,7 @@ import head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold"><h1>get 404d</h1>
+    <div className="text-3xl font-bold">get 404d
     </div>
   )
 }
