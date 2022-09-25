1bcf7c57b27790504e120b94c41af88ed607620f
danielhdev <d.huebschmann@tutanota.com>
2022-09-25 15:22:12 +0200

    update sunday

---
+++ 404d/pages/index.js
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import head from 'next/head'
+
+export default function Home() {
+  return (
+    <div className="text-3xl font-bold"><h1>get 404d</h1>
+    </div>
+  )
+}
