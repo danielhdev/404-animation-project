3f44a160138f6e415a914636074942d99d3993e3
danielhdev <d.huebschmann@tutanota.com>
2022-09-26 17:22:41 +0200

    update Mo

--- 404d/pages/index.js
+++ 404d/pages/index.js
@@ -1,9 +1,17 @@
 import Head from 'next/head'
-import head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="text-3xl font-bold">get 404d
-    </div>
+    <>
+      <Head>
+
+      </Head>
+
+      <div className="text-3xl font-bold flex justify-center 
+      items-center h-screen">get 404d!
+      </div>
+
+
+    </>
   )
 }
