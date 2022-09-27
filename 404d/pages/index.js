c5ede0dfcb477335136476d9269aae9541c2cab5
danielhdev <d.huebschmann@tutanota.com>
2022-09-27 13:16:59 +0200

    update Di

--- 404d/pages/index.js
+++ 404d/pages/index.js
@@ -7,26 +7,42 @@ export default function Home() {
       <Head>
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
-      <section className="mt-4 mx-auto grid grid-rows-auto md:grid-cols-2 gap-5 justify-items-center">
+      <section className="mt-4 mx-auto grid grid-rows-auto md:grid-cols-2 gap-6 
+      justify-items-center">
+        <article className='flex justify-center items-center'>
+          <p className="font-[Anton] text-[3rem] text-left">
+            A collection of page <br />not found pages.<br />Get 404d!</p>
+        </article>
+
+
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
+        </div>
+        <div id="card" className="cardcss">
+          <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
       </section>
-
-
     </>
   )
 }
