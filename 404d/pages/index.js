fdd4b7536cdb9aacf7e15200a02a7b9cae7d8974
danielhdev <d.huebschmann@tutanota.com>
2022-09-30 16:05:56 +0200

    update Friday

--- 404d/pages/index.js
+++ 404d/pages/index.js
@@ -9,14 +9,14 @@ export default function Home() {
         <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
       </Head>
 
-      <section className="mt-[100px] mx-auto grid grid-rows-auto md:grid-cols-2 gap-y-[50px] 
-      justify-items-center">
+      <section className="mt-[100px] mx-auto grid grid-rows-auto grid-cols-1 gap-y-[50px] 
+      md:gap-y-[150px] justify-items-center">
         <article className='flex justify-center items-center'>
           <p className="font-[Anton] text-[2.5rem] text-zinc-50 text-left px-20 ">
             A collection of animated 404 pages.</p>
         </article>
         <Link href="http://localhost:3000/pugnotfound">
-          <motion.div id="card1" className="cardcss md:mt-20"
+          <motion.div id="card1" className="cardcss"
             whileHover={{ x: [-5, 2, -12, 0] }}
             transition={{ duration: 1.5, times: [0.1, 0.2, 0.1, 0.3] }}
           >
@@ -31,25 +31,25 @@ export default function Home() {
             <h2 className="font-[Anton] text-orange-400">VINTAGE!</h2>
           </motion.div>
         </Link>
-        <div id="card" className="cardcss md:mt-20">
+        <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
-        <div id="card" className="cardcss md:mt-20">
+        <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
-        <div id="card" className="cardcss md:mt-20">
+        <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
         <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
-        <div id="card" className="cardcss md:mt-20">
+        <div id="card" className="cardcss">
           <h2 className="font-[Anton] text-zinc-50">PUG!</h2>
         </div>
       </section>
